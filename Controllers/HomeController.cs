9e61e6103590dc7cdce15c53cfb32f716bc723ce
jill <jill@epicodus.com>
2016-01-08 16:56:50 -0800

    Add working file upload feature to upload folder

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Http;
+using System.IO;
+using Microsoft.Net.Http.Headers;
+using System;
+
+namespace FileUpload01.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IHostingEnvironment _environment;
+
+        public HomeController(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(IFormFile file)
+        {
+            try
+            {
+                var uploads = Path.Combine(_environment.WebRootPath,"uploads");
+                var fileName = ContentDispositionHeaderValue.Parse(file.ContentDisposition).FileName.Trim('"');
+                //var fileName = "octopus.jpg";
+                await file.SaveAsAsync(Path.Combine(uploads, fileName));
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Write(ex);
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/File.cs
@@ -0,0 +1,12 @@
+﻿namespace FileUpload01.Models
+{
+    using System;
+    using System.Collections.Generic;
+
+    public partial class Image
+    {
+        public int Id { get; set; }
+        public string ImagePath { get; set; }
+
+    }
+}
